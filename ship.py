0fad8af3580bfe414e48b4393f781fd72ae55fa8
Ryan <ryan_leckich@baylor.edu>
2022-02-26 13:20:07 -0600

    ship update

--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship:
     "A class to manage the ship"
 
     def __init__(self, ai_game):
-        "Initialize the ship and st its starting postion"
+        "Initialize the ship and at its starting postion"
         self.screen = ai_game.screen
         self.screeen_rect = ai_game.screen.get_rect()
 
@@ -15,6 +15,14 @@ class Ship:
 
         # start each new ship at bottom center of screen
         self.rect.midbottom = self.screen_rct.midbottom
+
+        # movement flag
+        self.moving_right = False
+
+    def update(self):
+        "Update the ship's postion based on the movement flag"
+        if self.moving_right:
+            self.rect.x += 1
 
     def bltime(self):
         "Draw ship at its current location"
