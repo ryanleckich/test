5dcb998304387f1135ba1e8002eed66cfe7e77ea
Ryan <ryan_leckich@baylor.edu>
2022-02-26 21:59:04 -0600

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,15 +8,15 @@ from ship import Ship
 
 
 class AlienInvasion:
-    "Overall Class to manage game assets and behavior"
+    """Overall Class to manage game assets and behavior"""
 
     def __init__(self):
-        "Initialize the game, and create game resources."
+        """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
+            ((self.settings.screen_width, self.settings.screen_height))
         )
         pygame.display.set_caption("Alien Invasion")
 
@@ -32,7 +32,6 @@ class AlienInvasion:
             self._update_screen()
 
     def _check_events(self):
-        "Respond to keypresses and mouse events."
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -45,8 +44,8 @@ class AlienInvasion:
             # redraw the screen during each pass through the loop
 
     def _update_screen(self):
-        "Update images on the screen and flip to the new screen"
-        self.screen.fill(self.settings.bg_color)
+        # Update images on the screen and flip to the new screen
+        self.screen.fill(self.bg_color)
         self.ship.bltime()
 
         # MAke the most recently drawwn screen visible.
